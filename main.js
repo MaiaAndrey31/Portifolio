1662b6b8f7d2a7c01aa6c8cf44bd77e3beb7d3a9
MaiaAndrey31 <maiaandrey31@outlook.com>
2023-01-17 21:33:31 -0300

    the new

---
+++ main.js
@@ -0,0 +1,31 @@
+const backToTopButton = document.querySelector('a#backToTopButton')
+
+
+window.addEventListener('scroll', showBackToTopButtonOnScroll)
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 300) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+ScrollReveal({
+  origin: 'top',
+  distance: '100px',
+  duration: 2500
+}).reveal(`
+.portifolio,
+.menu,
+#sobre img,
+#sobre h2,
+#sobre p,
+#sobre a,
+.totem,
+.card,
+#knowledge,
+.contato button,
+.contato a,
+.contato input,
+.contato textarea`)
